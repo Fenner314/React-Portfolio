aa52c1558c737dda32f8bfbb4cec1737cdffcefc
Fenner314 <jacobgfenner@gmail.com>
2021-09-18 21:03:36 -0400

    add node, express, and mysql to languages

--- src/components/devPage/imgImports.js
+++ src/components/devPage/imgImports.js
@@ -1,43 +1,43 @@
-import homeGray from "../../utilities/home-icon.png";
-import homeGreen from "../../utilities/home-icon-green.png";
-import aboutGray from "../../utilities/about-icon.png";
-import aboutGreen from "../../utilities/about-icon-green.png";
-import projectsGray from "../../utilities/projects-icon.png";
-import projectsGreen from "../../utilities/projects-icon-green.png";
-import contactGray from "../../utilities/contact-icon.png";
-import contactGreen from "../../utilities/contact-icon-green.png";
-import githubGray from "../../utilities/github-gray.png";
-import githubGreen from "../../utilities/github-green.png";
-import linkedinGray from "../../utilities/linkedin-gray.png";
-import linkedinGreen from "../../utilities/linkedin-green.png";
-import logoName250 from "../../utilities/logoName250.png";
-import logo250 from "../../utilities/logo250.png";
-import resumeGray from "../../utilities/download-gray32.png";
-import resumeGreen from "../../utilities/download32.png";
-import cancelGray from "../../utilities/cancel-gray.png";
-import cancelGreen from "../../utilities/cancel-green.png";
-import landingBackground from "../../utilities/LandingBackground-desktop.jpg";
-import resume from "../../utilities/JacobFenner_Resume.pdf";
+import homeGray from '../../utilities/home-icon.png'
+import homeGreen from '../../utilities/home-icon-green.png'
+import aboutGray from '../../utilities/about-icon.png'
+import aboutGreen from '../../utilities/about-icon-green.png'
+import projectsGray from '../../utilities/projects-icon.png'
+import projectsGreen from '../../utilities/projects-icon-green.png'
+import contactGray from '../../utilities/contact-icon.png'
+import contactGreen from '../../utilities/contact-icon-green.png'
+import githubGray from '../../utilities/github-gray.png'
+import githubGreen from '../../utilities/github-green.png'
+import linkedinGray from '../../utilities/linkedin-gray.png'
+import linkedinGreen from '../../utilities/linkedin-green.png'
+import logoName250 from '../../utilities/logoName250.png'
+import logo250 from '../../utilities/logo250.png'
+import resumeGray from '../../utilities/download-gray32.png'
+import resumeGreen from '../../utilities/download32.png'
+import cancelGray from '../../utilities/cancel-gray.png'
+import cancelGreen from '../../utilities/cancel-green.png'
+import landingBackground from '../../utilities/LandingBackground-desktop.jpg'
+import resume from '../../utilities/Fenner_Resume.pdf'
 
 export {
-    homeGray,
-    homeGreen,
-    aboutGray,
-    aboutGreen,
-    projectsGray,
-    projectsGreen,
-    contactGray,
-    contactGreen,
-    githubGray,
-    githubGreen,
-    linkedinGray,
-    linkedinGreen,
-    logoName250,
-    logo250,
-    resumeGray,
-    resumeGreen,
-    cancelGray,
-    cancelGreen,
-    landingBackground,
-    resume
+	homeGray,
+	homeGreen,
+	aboutGray,
+	aboutGreen,
+	projectsGray,
+	projectsGreen,
+	contactGray,
+	contactGreen,
+	githubGray,
+	githubGreen,
+	linkedinGray,
+	linkedinGreen,
+	logoName250,
+	logo250,
+	resumeGray,
+	resumeGreen,
+	cancelGray,
+	cancelGreen,
+	landingBackground,
+	resume,
 }
